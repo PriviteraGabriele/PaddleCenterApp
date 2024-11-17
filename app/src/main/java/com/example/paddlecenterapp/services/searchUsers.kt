3378abf55235fce700385bbfed538a2f08ddee1a
PriviteraGabriele <gabriele.pr.01@gmail.com>
2024-11-17 19:32:52 +0100

    feat(SearchPage): improved user search function

--- app/src/main/java/com/example/paddlecenterapp/services/searchUsers.kt
+++ app/src/main/java/com/example/paddlecenterapp/services/searchUsers.kt
@@ -10,29 +10,26 @@ suspend fun searchUsers(query: String, authViewModel: AuthViewModel): List<User>
     val userList = mutableListOf<User>()
 
     val currentUser = authViewModel.getCurrentUser()
-
-    // Ottieni il DataSnapshot dalla query
     val snapshot = databaseRef.get().await()
 
     for (userSnapshot in snapshot.children) {
         val user = userSnapshot.getValue(User::class.java)
-        if (user != null) {
-            if (currentUser != null) {
-                if (user.email != currentUser.email) {
-                    val firstName = user.firstName
-                    val lastName = user.lastName
+        if (user != null && currentUser != null) {
+            if (user.email != currentUser.email) {
+                val firstName = user.firstName
+                val lastName = user.lastName
 
-                    // Controlla se il query è presente nel firstName o nel lastName
-                    if (firstName.contains(query, ignoreCase = true) || lastName.contains(
-                            query,
-                            ignoreCase = true
-                        )
-                    ) {
-                        userList.add(user)
-                    }
+                if(query.isEmpty()){
+                    userList.add(user)
+                }
+                else if (firstName.startsWith(query, ignoreCase = true) ||
+                    lastName.startsWith(query, ignoreCase = true)
+                ) {
+                    userList.add(user)
                 }
             }
         }
     }
     return userList
 }
+
