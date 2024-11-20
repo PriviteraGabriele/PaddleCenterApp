cda8831feedae06139acb908f80b580ea5f4f506
PriviteraGabriele <gabriele.pr.01@gmail.com>
2024-11-20 01:21:25 +0100

    committozzo pazzo

--- app/src/main/java/com/example/paddlecenterapp/pages/HomePage.kt
+++ app/src/main/java/com/example/paddlecenterapp/pages/HomePage.kt
@@ -32,6 +32,10 @@ fun HomePage(modifier: Modifier = Modifier, navController: NavController) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(text = "Home Page", fontSize = 32.sp)
+            Text(text = "- Aggiugere lista prenotazioni attive (da oggi in poi)", fontSize = 12.sp)
+            Text(text = "- Gestire update e delete prenotazione", fontSize = 12.sp)
+            Text(text = "- update dei soli partecipanti (caso caio non può più venire (cagarello a fischio) e si aggiungo pippo al suo posto)", fontSize = 12.sp)
+
         }
     }
 }
